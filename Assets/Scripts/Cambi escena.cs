454eb630993c76d8fa709d4569b6a7565d6bab29
Enrique Pons Martínez <enriquepons@ceroca.cat>
2025-10-30 16:13:22 +0100

    Escena Fi
    Fer escena fi i fer que  el boto et porti al menú

--- Assets/Scripts/Cambi escena.cs
+++ Assets/Scripts/Cambi escena.cs
@@ -4,10 +4,17 @@ using UnityEngine.SceneManagement;
 public class Cambiescena : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public void canviarInici(){
-    SceneManager.LoadScene("Inici");    
-}
-public void Exit(){
-    Application.Quit();
-}
+    public void canviarInici()
+    {
+        SceneManager.LoadScene("Inici");
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+public void canviarMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
 }
