310b35680f4320a9bb21cf8416812a5e1c365df3
Eric Díaz Bueno <ericdiaz@ceroca.cat>
2025-11-20 20:20:42 +0100

    Final del joc

--- Assets/Scripts/Mecaniques/Selected-deselected/Puzzle Final/controlPuzzleFInal.cs
+++ Assets/Scripts/Mecaniques/Selected-deselected/Puzzle Final/controlPuzzleFInal.cs
@@ -1,41 +1,30 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class controlPuzzleFinal : MonoBehaviour
 {
-    // Nom dels objectes que ha de tenir el jugador
     private HashSet<string> objectesRecollits = new HashSet<string>();
     private string[] objectesCorrectes = { "Biblia", "Espelma", "Caliz", "Crucifix" };
 
+    public FinalHistoria finalHistoria; // Assigna-ho al Inspector
+
     public void AgafarObjecte(string nomObjecte)
     {
-        // Només afegeix si és correcte i encara no està recollit
         if (System.Array.Exists(objectesCorrectes, x => x == nomObjecte))
         {
-            if (!objectesRecollits.Contains(nomObjecte))
-            {
-                objectesRecollits.Add(nomObjecte);
-                Debug.Log("Objecte recollit: " + nomObjecte);
-            }
-        }
-        else
-        {
-            Debug.Log("Objecte incorrecte: " + nomObjecte);
+            objectesRecollits.Add(nomObjecte);
         }
     }
 
-    // Aquesta funció la crida l’altar quan el jugador interactua
     public void ComprovarAltar()
     {
         if (objectesRecollits.Count == objectesCorrectes.Length)
         {
-            Debug.Log("Tots els objectes correctes! Enviant a Menu...");
-            SceneManager.LoadScene("Menu");
+            finalHistoria.IniciarHistoria();
         }
         else
         {
-            Debug.Log("Falten objectes per completar el puzle!");
+            Debug.Log("Encara falten objectes.");
         }
     }
 }
